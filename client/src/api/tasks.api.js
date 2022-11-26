d7373c35e157548c8d9bf2deb0e54c4494a1e5f6
FedeBayer <fbayerque@alumnos.exa.unicen.edu.ar>
2022-11-26 15:49:11 -0300

    Added feature delete task

--- client/src/api/tasks.api.js
+++ client/src/api/tasks.api.js
@@ -7,3 +7,7 @@ export const getTasksRequest = async (req, res) => {
 export const createTaskRequest = async (task) => {
   return await axios.post("http://localhost:4000/tasks", task);
 };
+
+export const deleteTaskRequest = async (id) => {
+  return await axios.delete(`http://localhost:4000/tasks/${id}`);
+};
