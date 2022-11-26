4bbcd62e7e528364492ba586b36a2883b3491a9f
FedeBayer <fbayerque@alumnos.exa.unicen.edu.ar>
2022-11-26 18:31:32 -0300

    Added build config

--- server/index.js
+++ server/index.js
@@ -1,17 +1,20 @@
-import cors from "cors";
 import express from "express";
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
 import { PORT } from "./config.js";
 import indexRoutes from "./routes/index.routes.js";
 import taskRoutes from "./routes/task.routes.js";
 
 const app = express();
-
-app.use(cors());
+const __dirname = dirname(fileURLToPath(import.meta.url));
 /*cors({
     origin: "http://localhost:5173",
   })*/
 app.use(express.json());
 app.use(indexRoutes);
 app.use(taskRoutes);
+
+app.use(express.static(join(__dirname, "../client/dist")));
+
 app.listen(PORT);
 console.log(`Server listening on port ${PORT}`);
