7a788238926ad1a60e36b4ab7e4e5fca73cb6b4f
FedeBayer <fbayerque@alumnos.exa.unicen.edu.ar>
2022-11-26 09:19:51 -0300

    Added task form with formik

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,18 @@
 import { Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
 import NotFound from "./pages/NotFound";
 import TaskForm from "./pages/TaskForm";
 import TaskPage from "./pages/TaskPages";
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<TaskPage />} />
-      <Route path="/new" element={<TaskForm />} />
-      <Route path="*" element={<NotFound />} />
-    </Routes>
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<TaskPage />} />
+        <Route path="/new" element={<TaskForm />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </>
   );
 }
 
--- client/src/pages/TaskForm.jsx
+++ client/src/pages/TaskForm.jsx
@@ -1,5 +1,39 @@
+import { Form, Formik } from "formik";
 function TaskForm() {
-  return <div>TaskForm</div>;
+  return (
+    <div>
+      <Formik
+        initialValues={{
+          title: "",
+          description: "",
+        }}
+        onSubmit={(values) => {
+          console.log(values);
+        }}
+      >
+        {({ handleChange, handleSubmit }) => (
+          <Form onSubmit={handleSubmit}>
+            <label>title</label>
+            <input
+              type="text"
+              name="title"
+              placeholder="Write a title"
+              onChange={handleChange}
+            />
+
+            <label>description</label>
+            <textarea
+              name="description"
+              rows="3"
+              placeholder="Write a description"
+              onChange={handleChange}
+            ></textarea>
+            <button type="submit">Save</button>
+          </Form>
+        )}
+      </Formik>
+    </div>
+  );
 }
 
 export default TaskForm;
